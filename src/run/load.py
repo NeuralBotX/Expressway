2e4a3360f89f2a44b59d6663fd5ce1513b666fad
Yunheng Wang <wangyunhenggxy@zjnu.edu.cn>
2023-12-24 18:07:18 +0800

    2023-1224-1807 add run catalogue.

---
+++ src/run/load.py
@@ -0,0 +1,9 @@
+from src.Tool.Buffer import SaveLoad
+import networkx as nx
+import matplotlib.pyplot as plt
+class_SaveLoad = SaveLoad()
+G , pos = class_SaveLoad.load_networkx('E:/expressway project/Data/Host file/', 'Composite_JTLJDP_Road_G')
+
+point_colors = list(nx.get_node_attributes(G, 'color').values())
+nx.draw(G, node_size=0.1, pos=pos, node_color=point_colors)
+plt.show()
